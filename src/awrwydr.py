07b7acc107b3109ca8f34929bea250ab84956317
c1m5j <antoni.chmiela@gmail.com>
2022-10-03 23:52:36 +0200

    fix printing intro message and exiting the repl

--- src/awrwydr.py
+++ src/awrwydr.py
@@ -11,20 +11,20 @@ VERSION = "0.1.0"
 message = f"""*~ awrwydr v{VERSION} ~*
 Enter an s-expression!"""
 
+print(message)
+
 lexer = Lexer()
 parser = Parser()
 vm = VM()
 
 while True:
     code = input(">> ")
-    if code in ["end", "(end)"]:
+    if code in ["", "end", "(end)"] or code.isspace():
         print("Diolch, hwyl!")
         break
     res = lexer.lex(code)
     if type(res) is str and res.startswith("\\"):
         stderr.write(res + "\n")
         break
-    # print(res)
     res = parser.parse(res)
-    # print(res)
     vm.run(res)
